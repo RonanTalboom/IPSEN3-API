f4bfe9eb2c8a62e7f5ff5e32d0c42b414a2ee7ee
ronantalboom <Ronantalboom@hotmail.com>
2016-12-08 19:20:54 +0100

    junit/api gedeelte van beheerder af

--- src/main/Resources/NotitieResource.java
+++ src/main/Resources/NotitieResource.java
@@ -1,69 +1,82 @@
-//package Resources;
-//
-//import java.util.*;
-//
-///**
-// *
-// */
-//public class NotitieResource {
-//
-//    /**
-//     * Default constructor
-//     */
-//    public NotitieResource() {
-//    }
-//
-//    /**
-//     *
-//     */
-//    public NotitieService service;
-//
-//    /**
-//     *
-//     */
-//    public NotitieService 1;
-//
-//    /**
-//     * @param service
-//     */
-//    public void NotitieResource(NotitieService service) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     *
-//     */
-//    public void retrieveAll() {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     */
-//    public void retrieve(int id) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param notitie
-//     */
-//    public void create(notitie notitie) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     * @param tag
-//     */
-//    public void update(int id, Tag tag) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     */
-//    public void delete(int id) {
-//        // TODO implement here
-//    }
-//
-//}
+package Resources;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.google.inject.Singleton;
+import main.Model.Notitie;
+import main.Services.NotitieService;
+import main.View;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+
+/**
+ *
+ */
+@Singleton
+@Path("/Notities")
+public class NotitieResource {
+
+
+    /**
+     *
+     */
+    public final NotitieService service;
+
+    @Inject
+    public void NotitieResource(NotitieService service) {
+        this.service = service;
+    }
+
+    /**
+     *
+     */
+    @GET
+    @JsonView(View.Public.class)
+    @RolesAllowed("GUEST")
+    public Collection<Notitie> retrieveAll() {
+        return service.getAll();
+    }
+
+    /**
+     * @param id
+     */
+    @GET
+    @Path("/{id}")
+    @JsonView(View.Public.class)
+    @RolesAllowed("GUEST")
+    public Notitie retrieve(int id) {
+        return service.get(id);
+    }
+
+    /**
+     * @param notitie
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Protected.class)
+    public void create(Notitie notitie) {
+        service.add(notitie);
+    }
+
+    /**
+     * @param id
+     * @param notitie
+     */
+    public void update(int id, Notitie notitie) {
+        service.update(id,notitie);
+    }
+
+    /**
+     * @param id
+     */
+    public void delete(int id) {
+        service.delete(id);
+    }
+
+}
--- src/main/Services/NotitieService.java
+++ src/main/Services/NotitieService.java
@@ -1,74 +1,74 @@
-//package Services;
-//
-//import java.util.*;
-//
-///**
-// *
-// */
-//public class NotitieService {
-//
-//    /**
-//     * Default constructor
-//     */
-//    public NotitieService() {
-//    }
-//
-//    /**
-//     *
-//     */
-//    public NotitieDAO dao;
-//
-//    /**
-//     *
-//     */
-//    public NotitieResource 1;
-//
-//    /**
-//     *
-//     */
-//    public NotitieDAO 1;
-//
-//    /**
-//     * @param dao
-//     */
-//    public void NotitieService(NotitieDAO dao) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     *
-//     */
-//    public void getAll() {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     */
-//    public void get(int id) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param notitie
-//     */
-//    public void add(Notitie notitie) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     * @param notitie
-//     */
-//    public void update(int id, notitie notitie) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param id
-//     */
-//    public void delete(int id) {
-//        // TODO implement here
-//    }
-//
-//}
+package main.Services;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import main.Model.Klant;
+import main.Model.Notitie;
+import main.Persistence.NotitieDAO;
+
+import java.util.*;
+
+/**
+ *
+ */
+@Singleton
+public class NotitieService {
+
+
+    /**
+     *
+     */
+    public NotitieDAO dao;
+
+    /**
+     * @param dao
+     */
+    @Inject
+    public void NotitieService(NotitieDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     *
+     */
+    public Collection<Notitie> getAll() {
+       dao.select();
+       return null;
+    }
+
+    /**
+     * @param notitieid
+     */
+    public void get(int notitieid) {
+        // TODO implement here
+    }
+
+    /**
+     * @param notitie
+     */
+    public void add(Notitie notitie, int klantid, int bedrijfid) {
+        // TODO implement here
+    }
+
+    /**
+     * @param id
+     * @param notitie
+     */
+    public void update(int id, Notitie notitie) {
+        // TODO implement here
+    }
+
+    /**
+     * @param id
+     */
+    public void delete(int id) {
+        // TODO implement here
+    }
+
+
+    public Collection<Notitie> getKlantNotitie(Klant klant){
+        dao.setKlant(klant);
+        dao.filterKlantNotitie();
+    }
+
+}
