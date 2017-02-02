9f321e39ecd07af894c3c3c16249c36f9cefa855
Shaban Jama <Shaban@dhcp-145.101.73.198.pool.hsleiden.nl>
2017-02-02 13:11:11 +0100

    tag bedrijven

--- src/main/Resource/BedrijfTagResource.java
+++ src/main/Resource/BedrijfTagResource.java
@@ -1,56 +1,72 @@
-//package Resources;
-//
-//import java.util.*;
-//
-///**
-// *
-// */
-//public class BedrijfTagResource {
-//
-//    /**
-//     * Default constructor
-//     */
-//    public BedrijfTagResource() {
-//    }
-//
-//    /**
-//     *
-//     */
-//    public BedrijfTagsService service;
-//
-//    /**
-//     *
-//     */
-//    public BedrijfTagService 1;
-//
-//    /**
-//     * @param service
-//     */
-//    public void BedrijfTagsResource(BedrijfTagService service) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param bedrijfid
-//     */
-//    public void retrieve(int bedrijfid) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param bedrijfid
-//     * @param tagid
-//     */
-//    public void create(int bedrijfid , int tagid ) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param bedrijfid
-//     * @param tagid
-//     */
-//    public void delete(int bedrijfid, int tagid) {
-//        // TODO implement here
-//    }
-//
-//}
+package main.Resource;
+
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.google.inject.Singleton;
+import main.Services.BedrijfTagService;
+import main.View;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+/**
+ *
+ */
+@Singleton
+@Path("/bedrijfTags")
+@Produces(MediaType.APPLICATION_JSON)
+public class BedrijfTagResource {
+
+    /**
+     *
+     */
+    public BedrijfTagService service;
+
+    /**
+     * Default constructor
+     * @param service
+     */
+    @Inject
+    public void BedrijfTagResource(BedrijfTagService service) {
+        this.service = service;
+    }
+
+    /**
+     *
+     */
+    @GET
+    @Path("/{id}")
+    @JsonView(View.Public.class)
+    @RolesAllowed("GUEST")
+    public Collection<Integer> retrieveAll(@PathParam("id") int id) {
+        System.out.println("bedrijfID: "+id);
+        return service.get(id);
+    }
+
+    /**
+     * @param id
+     */
+    @POST
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Public.class)
+    public void create( Collection<Integer> tagIDs, @PathParam("id") int id) {
+        System.out.println("bedrijfID: "+id);
+        service.add(tagIDs,id);
+    }
+
+    /**
+     * @param id
+     */
+    @DELETE
+    @Path("/{id}")
+    @RolesAllowed("GUEST")
+    public void delete(@PathParam("id") int id) {
+        service.delete(id);
+    }
+
+
+}
--- src/main/Services/BedrijfTagService.java
+++ src/main/Services/BedrijfTagService.java
@@ -1,61 +1,57 @@
-//package Services;
-//
-//import java.util.*;
-//
-///**
-// *
-// */
-//public class BedrijfTagService {
-//
-//    /**
-//     * Default constructor
-//     */
-//    public BedrijfTagService() {
-//    }
-//
-//    /**
-//     *
-//     */
-//    public BedrijfTagDAO dao;
-//
-//    /**
-//     *
-//     */
-//    public BedrijfTagResource 1;
-//
-//    /**
-//     *
-//     */
-//    public BedrijfTagDAO 1;
-//
-//    /**
-//     * @param dao
-//     */
-//    public void BedrijfTagService(BedrijfTagDAO dao) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param klantid
-//     */
-//    public void get(int klantid) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param bedrijfid
-//     * @param tagid
-//     */
-//    public void add(int bedrijfid, int tagid) {
-//        // TODO implement here
-//    }
-//
-//    /**
-//     * @param bedrijfid
-//     * @param tagid
-//     */
-//    public void delete(int bedrijfid, int tagid) {
-//        // TODO implement here
-//    }
-//
-//}
+package main.Services;
+
+import com.google.inject.Inject;
+import javafx.collections.ObservableList;
+import main.Persistence.BedrijfTagDAO;
+
+import java.util.*;
+
+/**
+ *
+ */
+public class BedrijfTagService {
+
+    /**
+     *
+     */
+    public BedrijfTagDAO dao;
+
+    /**
+     * @param dao
+     */
+    @Inject
+    public void BedrijfTagService(BedrijfTagDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * @param bedrijfID
+     */
+    public ObservableList<Integer> get(int bedrijfID) {
+        dao.setBedrijfID(bedrijfID);
+        dao.select();
+        return dao.getTagIDs();
+
+    }
+
+    /**
+     * @param bedrijfID
+     * @param tagIDs
+     */
+    public void add(Collection<Integer> tagIDs, int bedrijfID) {
+        dao.setBedrijfID(bedrijfID);
+        dao.delete(bedrijfID);
+        for (Integer tagID: tagIDs) {
+            dao.setTagID(tagID);
+            dao.insert();
+        }
+    }
+
+    /**
+     * @param bedrijfID
+     */
+    public void delete(int bedrijfID) {
+        dao.delete(bedrijfID);
+    }
+
+}
